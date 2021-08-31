1840bba8f49b939170cebade3e8d3c72f3d7fc50
Tronke <gebhardpablo@gmail.com>
2021-08-31 15:04:34 -0400

    creando before en comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,20 @@
 class CommentsController < ApplicationController
+    before_action :find_article, only: [:create, :destroy]
     def create
-        @article = Article.find(params[:article_id])
-
         @comment = @article.comments.create(comment_params)
         redirect_to @article
     end
 
     def destroy
-        @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
         @comment.destroy
         redirect_to article_path(@article)
     end
 
     private
+        def find_article
+            @article = Article.find(params[:article_id])
+        end 
         def comment_params
             params.require(:comment).permit(:commenter, :body)
         end
